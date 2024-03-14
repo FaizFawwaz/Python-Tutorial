f871c817e1d93ace38b81347b821ea2bf48b66b1
MOHD FAIZ FAWWAZ BIN RAZAK <157676083+FaizFawwaz@users.noreply.github.com>
2024-03-15 07:27:15 +0800

    Add files via upload

---
+++ Tuto.py
@@ -0,0 +1,23 @@
+integers = [1,2,3,4,5]
+for number in integers:
+    print(number)  
+    
+integers = [1,2,3,4,5]    
+for Jelly in integers:
+    print(Jelly + Jelly)
+    
+    
+    
+ice_cream_dict = {'name': 'Faiz', 'weekly intakes':5, 'week favorite ice cream': ['mcc', 'chocolate']}
+for cream in ice_cream_dict.values():
+    print(cream)
+for key, value in ice_cream_dict.items():
+    print(key, '->', value)   
+    
+
+flavors = ['Vanilla', 'Chocolate', 'Cookie Dough']
+toppings = ['Hot Fudge', 'Oreo', 'Marshmallow']
+for one in flavors:
+    for two in toppings:
+        print(one, 'topped with', two)
+   
