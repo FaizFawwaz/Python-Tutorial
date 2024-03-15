e2510ee6e156b1e9651b60e2fab1c324f4324bd7
MOHD FAIZ FAWWAZ BIN RAZAK <157676083+FaizFawwaz@users.noreply.github.com>
2024-03-15 02:58:30 +0000

    new

---
+++ BMI.py
@@ -0,0 +1,9 @@
+weight =  int(input('Enter your weight in pounds: '))
+height =  int(input("Enter your height in inches: "))
+BMI = (weight * 703) / (height * height )
+print("Your BMI is: {:.2f}".format(BMI))
+
+type(height)
+
+x = 3
+print(x)
